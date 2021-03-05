ff7403b7e7f8f073ac67ee85a790679a514eab1e
Matheus Bruhns Bastos <matbbastos@gmail.com>
2021-03-04 22:27:57 -0300

    FEAT: Add parent elements in constructors

--- web/api/model.py
+++ web/api/model.py
@@ -47,6 +47,7 @@ class Company(db.Model):
     Args:
         cnpj (string): CNPJ number of the company (unique, 14 digits)
         name (string): full name of the company
+        parent (string): CNPJ or CPF number of the parent (person or company)
     """
 
     __tablename__ = "companies"
@@ -66,9 +67,14 @@ class Company(db.Model):
         backref=db.backref('children_company'),
         single_parent=True)
 
-    def __init__(self, cnpj, name):
+    def __init__(self, cnpj, name, parent):
         self.cnpj = cnpj
         self.name = name
+
+        if len(parent) == 14:
+            self.parent_cpf = parent
+        elif len(parent) == 18:
+            self.parent_cnpj = parent
 
     def save(self):
         db.session.add(self)
@@ -95,9 +101,10 @@ class Asset(db.Model):
 
     Args:
         id_ (integer): Identification number of the asset
-        description (string): general description of the asset
-        date_acquisition (datetime): date at which it was obtained
         value (numeric): estimated or market value
+        description (string): general description of the asset
+        date_acquisition (datetime): date at which it was obtained [OPTIONAL]
+        parent (string): CNPJ or CPF number of the parent (person or company)
     """
 
     __tablename__ = "assets"
@@ -113,11 +120,16 @@ class Asset(db.Model):
     parent_cnpj = db.Column(db.String(30), db.ForeignKey('companies.cnpj'), name='parent_cnpj', nullable=True)
     parent_cpf = db.Column(db.String(20), db.ForeignKey('people.cpf'), name='parent_cpf', nullable=True)
 
-    def __init__(self, id_, value, description, date_acquisition=None):
+    def __init__(self, id_, value, description, parent, date_acquisition=None):
         self.id_ = id_
         self.value = value
         self.description = description
         self.date_acquisition = date_acquisition
+
+        if len(parent) == 14:
+            self.parent_cpf = parent
+        elif len(parent) == 18:
+            self.parent_cnpj = parent
 
     def save(self):
         db.session.add(self)
